0eeb11b8df8d2bae2fdf571effc41463b1c08b9d
Shrey <shrey.dabhi23@gmail.com>
2020-10-04 16:25:55 +0530

    change project and app name

--- django-hasura-auth/django-hasura-auth/wsgi.py
+++ django-hasura-auth/django-hasura-auth/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tripmgmt project.
+WSGI config for django-hasura-auth project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tripmgmt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-hasura-auth.settings')
 
 application = get_wsgi_application()
